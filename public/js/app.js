78107e1fcdea7cce30b07899933c7f9fdeb95c51
Tenshi3 <jonmassey@gmail.com>
2016-05-30 17:46:05 +0100

    Asset weight gain

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,70 @@
-function options(){$.ajax({dataType:"json",url:"http://idea-battle.herokuapp.com/game"}).done(function(t,i,n){$("#left .panel-title").html(t.left.title),$("#left .description").html(t.left.description),$("#left button").data("id",t.left.id).click(function(){vote("#left")}),$("#right .panel-title").html(t.right.title),$("#right .description").html(t.right.description),$("#right button").data("id",t.right.id).click(function(){vote("#right")}),$("#vote").data("uuid",t.uuid),$("#right").transition({x:0,opacity:1,duration:500,easing:"out"}),$("#left").transition({x:0,opacity:1,duration:500,easing:"out"})})}function vote(t){$("#right.panel").transition({opacity:0,duration:500,easing:"out"}),$("#left.panel").transition({opacity:0,duration:500,easing:"out",complete:function(){$.ajax({method:"POST",dataType:"json",contentType:"application/json;",url:"http://idea-battle.herokuapp.com/game/vote",processData:!1,data:JSON.stringify({vote:$(t+" button").data("id"),uuid:$("#vote").data("uuid")})}).done(function(t,i,n){}).always(function(){$(".panel button").unbind(),$("#left").css({x:"-1000px"}),$("#right").css({x:"1000px"}),options()})}})}$(function(){options()});
+$(function() {
+    options();
+});
+
+function options() {
+    $.ajax({
+        dataType: "json",
+        url: "http://idea-battle.herokuapp.com/game"
+    }).done(function(data, textStatus, jqXHR){
+        //console.log(data);
+        $('#left .panel-title').html(data.left.title);
+        $('#left .description').html(data.left.description);
+        $('#left button').data('id', data.left.id).click(function() {
+            vote('#left');
+        });;
+
+        $('#right .panel-title').html(data.right.title);
+        $('#right .description').html(data.right.description);
+        $('#right button').data('id', data.right.id).click(function() {
+            vote('#right');
+        });
+        $('#vote').data('uuid', data.uuid);
+
+        $('#right').transition({
+            x: 0,
+            opacity: 1,
+            duration: 500,
+            easing: 'out'
+        });
+        $('#left').transition({
+            x: 0,
+            opacity: 1,
+            duration: 500,
+            easing: 'out',
+        });
+    });
+}
+
+function vote(e) {
+
+    $("#right.panel").transition({
+        opacity: 0,
+        duration: 500,
+        easing: 'out'
+    });
+
+    $("#left.panel").transition({
+        opacity: 0,
+        duration: 500,
+        easing: 'out',
+        complete: function() {
+            $.ajax({
+                method: "POST",
+                dataType: "json",
+                contentType: "application/json;",
+                url: "http://idea-battle.herokuapp.com/game/vote",
+                processData: false,
+                data: JSON.stringify({'vote': $(e + ' button').data('id'), 'uuid': $('#vote').data('uuid')})
+            }).done(function(data, textStatus, jqXHR){
+                console.log(data);
+            }).always(function() {
+                $(".panel button").unbind();
+                $("#left").css({x: '-1000px'});
+                $("#right").css({x: '1000px'});
+                options();
+            });
+        }
+    });
+}
+//# sourceMappingURL=app.js.map
